d126c450b56a0896d847a70c0473b8aa137e81ca
Avimistry4800 <avimistry4800@gmail.com>
2021-10-17 20:59:27 +0600

    adding db structure

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     mongoose
-        .connect(process.env.DB_URi, {
+        .connect(process.env.DB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
+            
         })
         .then((data) => {
             console.log(
---
+++ Backend/models/productModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Product name is required"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Product description is required"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"],
+        maxLength: [8, "Product price is too long"],
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    images: [
+        {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    }
+    ],
+   category: {
+       type:String,
+       required: [true, "Product category is required"],
+    },
+
+  stock: {
+    type: Number,
+    required: [true, "Product stock is required"],
+    maxLength: [4, "Product stock cannot exceed 4 caracters"],
+  },
+
+});
