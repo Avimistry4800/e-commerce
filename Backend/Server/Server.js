19ea4bea9c2695e5570133aad04c6ea53bb62bb1
Avimistry4800 <avimistry4800@gmail.com>
2021-11-15 21:30:24 +0600

    errorhandling basic done

--- Backend/Server/Server.js
+++ Backend/Server/Server.js
@@ -3,12 +3,32 @@ const app = require("../app");
 const dotenv = require("dotenv");
 const connectDatabase = require("../config/database");
 
+
+//Uncaught error handler
+
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Uncaught Execption");
+   process.exit(1);
+})
+
 // CONFIG
 dotenv.config({ path: "Backend/config/config.env" });
 
 // Connecting to the database
 connectDatabase(process.env.DB_URI);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+
+// Unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Unhandled Promise Rejection ");
+    server.close(() => {
+        process.exit(1); 
+    })
+
 });
--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -5,6 +5,14 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
+// Wrong MongoDB Id Error
+
+    if (err.name === "CastError") {
+message = `Resource not found with id of ${err.value}`;
+err= new ErrorHandler(message, 400);
+    }
+
+
     res.status(err.statusCode).json({
         success: false,
         message:err.message
