c02cae4e1959428edec69564ea6c43472f967fa8
Avimistry4800 <avimistry4800@gmail.com>
2021-11-15 20:54:34 +0600

    errorHandling class added

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const errorMiddleware = require("./middleware/error");
+
 // Route Imports
 const product = require("./routes/productRoute");
 
 app.use("/api/v1", product);
+
+//Middleware fot error handling
+
+app.use(errorMiddleware);
+
+
+
 
 module.exports = app;
--- Backend/utils/errorhandler.js
+++ Backend/utils/errorhandler.js
@@ -1,1 +1,11 @@
-class ErrorHandler extends  Error
+class ErrorHandler extends  Error{
+
+    constructor(message,statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports= ErrorHandler
