3321efcd6c2bb47a34fba41fc215ce298a93936f
Avimistry4800 <avimistry4800@gmail.com>
2021-11-25 01:56:21 +0600

    mongo id error handler added

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -12,6 +12,13 @@ message = `Resource not found with id of ${err.value}`;
 err= new ErrorHandler(message, 400);
     }
 
+// Mongoose Duplicate KEY Error
+
+if(err.code === 11000){
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+}
+
 
     res.status(err.statusCode).json({
         success: false,
