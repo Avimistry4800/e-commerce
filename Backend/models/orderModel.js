9ce7dc41779c755e1967c6a874979aa318b029eb
Avimistry4800 <avimistry4800@gmail.com>
2021-11-28 21:45:19 +0600

    Order Schema ready

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -14,10 +14,6 @@ const orderSchema = new mongoose.Schema({
             type: String, 
             required: true 
         },
-        zip: { 
-            type: String, 
-            required: true 
-        },
         country: { 
             type: String, 
             required: true 
@@ -31,4 +27,81 @@ const orderSchema = new mongoose.Schema({
             required: true 
         },
     },
+    orderItems: [
+        {
+            name:{
+                type: String,
+                required: true,
+            },
+            quantity:{
+                type: Number,
+                required: true,
+            },
+            price:{
+                type: Number,
+                required: true,
+            },
+            image:{
+                type: String,
+                required: true,
+            },
+            product:{
+                type: mongoose.Schema.ObjectId,
+                ref:"Product",
+                required: true,
+            },
+        },
+    ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    paymentInfo:{
+        id: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+    },
+    paidAt:{
+        type: Date,
+        required: true,
+    },
+    itemsPrice:{
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    taxPrice:{
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    shippingPrice:{
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    totalPrice:{
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    orderStatus:{
+        type: String,
+        required: true,
+        default: "Processing",
+    },
+    deliveredAt: Date,
+    ceratedAt:{
+        type:Date,
+        default: Date.now,
+
+    }
 });
+
+module.exports = mongoose.model("Order", orderSchema);  
