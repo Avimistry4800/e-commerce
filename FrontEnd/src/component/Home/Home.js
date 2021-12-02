002770337eb9f8eca01fdafcb93d17b6b0399f78
Avimistry4800 <avimistry4800@gmail.com>
2021-12-02 20:56:29 +0600

    show feature product and top home page section working with demo

--- FrontEnd/src/component/Home/Home.js
+++ FrontEnd/src/component/Home/Home.js
@@ -1,6 +1,18 @@
 import React, { Fragment } from 'react'
 import { CgMouse } from 'react-icons/cg'
+import Product from './Product.js'
 import  './Home.css'
+
+
+
+const product={
+    name: 'Redmi Note 8 Pro',
+    price: '$1299',
+    _id: '1',
+    images: [{
+        url: 'https://fdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-note-8-pro-0.jpg'
+    }]
+}
 
 const Home = () => {
     return (
@@ -21,6 +33,16 @@ const Home = () => {
         <h2 className="homeHeading">
             Featured Products
         </h2>
+        <div className="container" id="container">
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+            <Product product={product} />
+        </div>
 
        </Fragment>
     )
